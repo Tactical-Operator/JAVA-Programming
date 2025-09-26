5ed885f792c3a31c68eca1003e2c28e3c48722fa
ASHWIN JADHAV <ashwinj2110@gmail.com>
2025-09-26 11:52:34 +0530

    Added another approach of Magictriplet.java

--- GFG/magictriplet.java
+++ GFG/magictriplet.java
@@ -3,20 +3,42 @@ class  magictriplet
 {
     public static void main(String[] args)
     {
-       int[] arr={2,3,4,9,10,12,14,15};
+       int[] arr={1,2,3,4};
 
        int n=arr.length;
        int count=0;
+       
+    // Brute Force Method
+    //    for (int i = 0; i < n; i++) {
+    //         for (int j = i + 1; j < n; j++) {
+    //             for (int k = j + 1; k < n; k++) {
+    //                 if (arr[i] < arr[j] && arr[j] < arr[k]) {
+    //                     count++;
+    //                 }
+    //             }
+    //         }
+    //   }
 
-       for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                for (int k = j + 1; k < n; k++) {
-                    if (arr[i] < arr[j] && arr[j] < arr[k]) {
-                        count++;
-                    }
-                }
+
+    // Hold a Middle Elmenet and check smaller on left and greater on right
+    for(int j=1;j<=n-2;j++){
+        int greater=0;
+        int smaller=0;
+
+        for(int i=0;i<j;i++){
+            if(arr[i]<arr[j]){
+                smaller++;
             }
-      }
-      System.out.println(count);
+        }
+
+        for(int k=j+1;k<n;k++){
+            if(arr[k]>arr[j]){
+                greater++;
+            }
+        }
+        count+=smaller*greater;
+    }
+
+    System.out.println(count);
    }
 }
