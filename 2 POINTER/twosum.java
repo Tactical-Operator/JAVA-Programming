713da4b255d64e9638c06bccd6d0b9743b8a57c2
ASHWIN JADHAV <ashwinj2110@gmail.com>
2025-09-28 15:23:43 +0530

    Added twosum.java

---
+++ 2 POINTER/twosum.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class twosum 
+{
+    public static void main(String[] args)
+    {
+       int[] arr={15,7,2,11};
+       int target=9;
+       HashMap<Integer,Integer> map=new HashMap<>();
+
+       for(int i=0;i<arr.length;i++){
+
+        int complement=target-arr[i];
+
+        if(map.containsKey(complement)){
+            System.out.println((map.get(complement))+","+i);
+        }
+        map.put(arr[i],i);
+       }
+    
+    }
+}
