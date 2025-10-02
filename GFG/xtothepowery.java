fab5ef3eb11b8a23d6875daf0afbc0434f7dd770
ASHWIN JADHAV <ashwinj2110@gmail.com>
2025-10-02 16:24:59 +0530

    x^y.java code optimized

--- GFG/xtothepowery.java
+++ GFG/xtothepowery.java
@@ -2,23 +2,53 @@
 
 class xtothepowery {
     public static void main(String[] args) {
-        int n = 27;
-        boolean result = false;
-        int x=0,y=0;
+        int n = 525;
+        boolean found = false;
+        int base=0,exponent=0;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (Math.pow(i, j) == n) {
-                    result = true;
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = 0; j < n; j++) {
+        //         if (Math.pow(i, j) == n) {
+        //             result = true;
                     
-                    x=i;
-                    y=j;
+        //             base=i;
+        //             exponent=j;
+        //             break;
+                    
+        //         }
+        //     }
+        //     if(result) break;
+        // }
+
+        for(int i=2;i*i<=n;i++){
+            int power=i*i;
+            int j=2;
+
+            while(power<=n){
+                if(power ==n){
+                    found =true;
+                    base=i;
+                    exponent=j;
                     break;
                     
                 }
+                j++;
+                power*=i;
             }
-            if(result) break;
+            if (found){
+                break;
+            }
         }
-        System.out.println(result+" because "+x+"^"+y+" is equal to "+n);
+
+
+
+
+
+        if(found){
+        System.out.println(found+" because "+base+"^"+exponent+" is equal to "+n);
+        }
+        else{
+            System.out.println(found);
+        }
     }
 }
