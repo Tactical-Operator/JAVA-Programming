19c1876e3846133c201f670b1c0da7f48889a987
ASHWIN JADHAV <ashwinj2110@gmail.com>
2025-10-11 21:38:22 +0530

    Added Kardane.java code

--- GFG/kardane.java
+++ GFG/kardane.java
@@ -1,8 +1,12 @@
+// find the subarray from an array which has the maximum sum using bRUte FoRce and KArdane's Algorithm
+
+
+
 import java.util.*;
 
 class kardane {
     public static void main(String[] args) {
-        //int[] arr = {-2,-3,4,-1,-2,1,5,-3};
+        //int[] arr = {2,6,8,1,4};
         //Brute Force approach O(n^2) Time Complexity
         // int left = 0, right = 0;
         // int sum = 0;
@@ -25,16 +29,16 @@ class kardane {
 
         // Optimized Code with Kardane's Algorithm with O(n) time complexity
 
-        int arr[]={-2,-3,4,-1,-2,1,5,-3};
+        int arr[]={2,6,8,1,4};
 
         int currentSum=arr[0];
         int maximumSum=arr[0];
          for (int i = 1; i < arr.length; i++) {
             // either start new subarray at arr[i], or extend previous one
-            currentSum = Math.max(arr[i], currentSum + arr[i]);
+            currentSum = Math.min(arr[i], currentSum + arr[i]);
 
             // update max if the current sum is better
-            maximumSum = Math.max(maximumSum, currentSum);
+            maximumSum = Math.min(maximumSum, currentSum);
 
 
          }
