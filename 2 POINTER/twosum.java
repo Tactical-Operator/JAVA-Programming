b92bb267b78cc68d7f6849883b503030c44abee3
ASHWIN JADHAV <ashwinj2110@gmail.com>
2025-10-11 13:05:26 +0530

    Added new files

--- 2 POINTER/twosum.java
+++ 2 POINTER/twosum.java
@@ -4,7 +4,7 @@ class twosum
     public static void main(String[] args)
     {
        int[] arr={15,7,2,11,14,12};
-       int target=25;
+       int target=9;
        HashMap<Integer,Integer> map=new HashMap<>();
 
        for(int i=0;i<arr.length;i++){
--- GFG/kardane.java
+++ GFG/kardane.java
@@ -2,26 +2,44 @@ import java.util.*;
 
 class kardane {
     public static void main(String[] args) {
-        int[] arr = {5,4,1,7,8};
-        int left = 0, right = 0;
-        int sum = 0;
-        int maximum_sum = arr[0];
+        //int[] arr = {-2,-3,4,-1,-2,1,5,-3};
+        //Brute Force approach O(n^2) Time Complexity
+        // int left = 0, right = 0;
+        // int sum = 0;
+        // int maximum_sum = arr[0];
 
-        for (int i = 0; i < arr.length; i++) {
-            sum = 0;
-            left = i;
-            while (left < arr.length) {
+        // for (int i = 0; i < arr.length; i++) {
+        //     sum = 0;
+        //     left = i;
+        //     while (left < arr.length) {
 
-                sum = sum + arr[left];
-                left++;
-                if (sum > maximum_sum) {
-                    maximum_sum = sum;
-                }
+        //         sum = sum + arr[left];
+        //         left++;
+        //         if (sum > maximum_sum) {
+        //             maximum_sum = sum;
+        //         }
+        //     }
+        // }
+        // System.out.println(maximum_sum);
 
-            }
 
-        }
-        System.out.println(maximum_sum);
+        // Optimized Code with Kardane's Algorithm with O(n) time complexity
+
+        int arr[]={-2,-3,4,-1,-2,1,5,-3};
+
+        int currentSum=arr[0];
+        int maximumSum=arr[0];
+         for (int i = 1; i < arr.length; i++) {
+            // either start new subarray at arr[i], or extend previous one
+            currentSum = Math.max(arr[i], currentSum + arr[i]);
+
+            // update max if the current sum is better
+            maximumSum = Math.max(maximumSum, currentSum);
+
+
+         }
+        System.out.println(maximumSum);
+        // 
 
     }
 }
