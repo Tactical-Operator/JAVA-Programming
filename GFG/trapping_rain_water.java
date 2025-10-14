6126d238a3557bdd7ee223c821264a90f07a6cdd
ASHWIN JADHAV <ashwinj2110@gmail.com>
2025-10-14 21:06:12 +0530

    Added new changes

--- GFG/trapping_rain_water.java
+++ GFG/trapping_rain_water.java
@@ -16,13 +16,13 @@ import java.util.*;
 
 class trapping_rain_water {
     public static void main(String[] args) {
-        int[] arr = { 2,1,5,3,1,0,4};
+        int[] arr = {28,13,47,33,83,72,19,86,37,4,28,22,83,32,9,63,68,92,8,0,39,78,45,33,30,71,6,37,39,35,86,48,13,16,76,34,70,64,31,92,74,0,16,78,57,92,10,96,21,65,20,25,14,34,23,21,96,86,62,82,67,41,32,73,16,9,53,78,66,6,47,69,78,1,14,42,36,17,49,79,59,84,87,61,39,81,70,56,48,5,75,19,58};
         int max_height = 0;
         int n = arr.length;
         int max_height_index = 0;
 
         for (int i = 0; i < n; i++) {
-            if (max_height < arr[i]) {
+            if (max_height <= arr[i]) {
                 max_height = arr[i];
                 max_height_index = i;
             }
@@ -41,13 +41,18 @@ class trapping_rain_water {
         int water_blocks=0;
 
         for(int i=left;i>0;i--){
+
+            if(arr[i]<left_boundary){
             difference=left_boundary-arr[i];
             water_blocks+=difference;
+            }
 
         }
         for(int j=right;j<n;j++){
+            if(arr[j]<right_boundary){
             difference=right_boundary-arr[j];
             water_blocks+=arr[j];
+            }
         }
 
         System.out.println(water_blocks);
