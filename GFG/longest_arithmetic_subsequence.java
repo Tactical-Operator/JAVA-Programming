7238de01758a7ac9d9fcba0e04e39c4cc68ec4f6
ASHWIN JADHAV <ashwinj2110@gmail.com>
2025-11-04 19:43:40 +0530

    Added new changes

---
+++ GFG/longest_arithmetic_subsequence.java
@@ -0,0 +1,18 @@
+// Input: arr[] = [10, 5, 2, 7, 1, -10], k = 15
+// Output: 6
+// Explanation: Subarrays with sum = 15 are [5, 2, 7, 1], [10, 5] and [10, 5, 2, 7, 1, -10]. The length of the longest subarray with a sum of 15 is 6.
+// Input: arr[] = [-5, 8, -14, 2, 4, 12], k = -5
+// Output: 5
+// Explanation: Only subarray with sum = -5 is [-5, 8, -14, 2, 4] of length 5.
+// Input: arr[] = [10, -10, 20, 30], k = 5
+// Output: 0
+// Explanation: No subarray with sum = 5 is present in arr[].
+
+import java.util.*;
+class longest_arithmetic_subsequence 
+{
+    public static void main(String[] args)
+    {
+       int[] arr={1,7,10,13,14,19};
+    }
+}
