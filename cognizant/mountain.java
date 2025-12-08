0d7754ba967a4bdd14b40e6068a9b1a641876ab0
ASHWIN JADHAV <ashwinj2110@gmail.com>
2025-12-08 09:58:42 +0530

    cODE ADDED

---
+++ cognizant/mountain.java
@@ -0,0 +1,46 @@
+// You are a part of mountain expedition team analysing the altitude changes during your journey.
+// Each checkpoint you pass is recorded with an altitude value and the expedition team wants to find 
+// the highestpeak encountered during the trek.A peak is declined as a sequence of altitudes that 
+// first strictly increases,reahces a summit(the highest point),and then strictly decreases.
+// The peak must have at least three checkpoints:
+// * The first one where the altitude is increasing
+// * the second one representing the summit
+// * the last one where the altitude is decreasing
+
+// You are given an integer array A of size N where each element represents the altitude at a checkpoint.
+// Your task is to help the team find and return an integer value representing the maximum altitude sum 
+// of any peak array during the trek.
+// 
+// INPUT SPECIFICATION
+// input1: An integer N representing the number of checkpints during the trek.
+// input2: An integer array a,where each element represents the altitude at a checkpoint.
+//
+// Output Specification
+// Return an integer value representing the maximum altitude sum of any peak subarray during the trek
+//
+// Example 1:
+// input1: 7
+// input2: {1,2,3,4,3,2,1}
+//
+// output : 16
+//
+// Example 2:
+// input1: 8
+// input2: {5,1,2,3,2,1,6,7}
+//
+// output : 9
+
+
+import java.util.*;
+class mountain 
+{
+    public static void main(String[] args)
+    {
+       Scanner sc=new Scanner(System.in);
+       int mountains=0;
+       int heights=0;
+       int peak=0;
+       int sum=0;
+
+    }
+}
