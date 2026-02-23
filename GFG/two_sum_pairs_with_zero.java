da85abaa976718a583bfebdb021d2be8082f06f6
ASHWIN JADHAV <ashwinj2110@gmail.com>
2026-02-23 22:10:23 +0530

    new problem added

---
+++ GFG/two_sum_pairs_with_zero.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class two_sum_pairs_with_zero 
+{
+    public static void main(String[] args)
+    {
+       Scanner sc=new Scanner(System.in);
+       ArrayList <ArrayList<Integer>> result=new ArrayList<>();
+       ArrayList<Integer> pair= new ArrayList<>();
+       pair.add(-1);
+       pair.add(1);
+       result.add(pair);
+       System.out.println(result);
+
+    }
+}
