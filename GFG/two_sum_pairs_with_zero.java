e52740d0787499f10963888406939d9730d027ad
ASHWIN JADHAV <ashwinj2110@gmail.com>
2026-02-26 14:22:46 +0530

    New Changes Made

--- GFG/two_sum_pairs_with_zero.java
+++ GFG/two_sum_pairs_with_zero.java
@@ -5,11 +5,21 @@ class two_sum_pairs_with_zero
     {
        Scanner sc=new Scanner(System.in);
        ArrayList <ArrayList<Integer>> result=new ArrayList<>();
-       ArrayList<Integer> pair= new ArrayList<>();
-       pair.add(-1);
-       pair.add(1);
-       result.add(pair);
-       System.out.println(result);
+       int arr[]={-1, 0, 1, 2, -1, -4};
+       Arrays.sort(arr);
+       int left=0,right=arr.length-1;
+       while(left<right){
 
+        int sum =arr[left]+arr[right];
+        if(sum==0){
+            ArrayList<Integer> pair=new ArrayList<>();
+            pair.add(left);
+            pair.add(right);
+            result.add(pair);
+
+            int leftvalue=arr[left];
+            int rigthvalue=arr[right];
+         }
+       }
     }
 }
