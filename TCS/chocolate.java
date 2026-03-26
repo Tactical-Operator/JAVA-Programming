39b145d36ffea18fa466efb10511b4b82f438237
ASHWIN JADHAV <ashwinj2110@gmail.com>
2026-03-26 13:05:11 +0530

    updated chocolate.java

--- TCS/chocolate.java
+++ TCS/chocolate.java
@@ -4,19 +4,29 @@ class chocolate {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = 8;
-        int []result_array = new int[n];
+        int[] result_array = new int[n];
         int arr[] = { 4, 5, 0, 1, 9, 0, 5, 0 };
         int l = arr.length;
         int result_array_index = 0;
-        for(int i=0;i<l;i++){
-            if(arr[i]!=0){
-                result_array[result_array_index++]=arr[i];
+        // for(int i=0;i<l;i++){
+        // if(arr[i]!=0){
+        // result_array[result_array_index++]=arr[i];
+        // }
+        // }
+        // while(result_array_index<n){
+        // result_array[result_array_index++]=0;
+        // }
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != 0) {
+                int temp = arr[result_array_index];
+                arr[result_array_index] = arr[i];
+                arr[i] = temp;
+                result_array_index++;
+
             }
         }
-        while(result_array_index<n){
-            result_array[result_array_index++]=0;
-        }
-        System.out.println(Arrays.toString(result_array));
+
+        System.out.println(Arrays.toString(arr));
 
     }
 }
